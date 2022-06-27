df1f182afba1e1097e05cf55fc57bd91415eb4c9
ducnv2509 <ducit2509@gmail.com>
2022-06-27 21:51:26 +0700

    up project base

---
+++ src/main/java/lbc/com/longbiencomputer/LongBienComputerApplication.java
@@ -0,0 +1,13 @@
+package lbc.com.longbiencomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongBienComputerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongBienComputerApplication.class, args);
+    }
+
+}
